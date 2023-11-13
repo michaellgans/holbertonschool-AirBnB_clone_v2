c566d8e7297decb9cba50d7d02c0b9b158e0457d
michaellgans <michaellgans@Victoria>
2023-11-13 14:41:04 -0600

    Creates task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Flask document for task 5 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Displays a greeting """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def index1():
+    """ Displays HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def index2(text):
+    """ Displays C and then specific text from the user """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def index3(text="is cool"):
+    """ Displays Python and then is cool as default """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def index4(n):
+    """ Using a route parameter with a converter """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def index5(n):
+    """ Uses a route parameter with a converter """
+    return "<html><body><h1>Number: {}</h1></body></html>".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
