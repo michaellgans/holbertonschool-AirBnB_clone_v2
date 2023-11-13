eac7cef2c23e68b2b56c91e8908fabd19c7a61ee
michaellgans <michaellgans@Victoria>
2023-11-13 16:07:16 -0600

    Updates engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -82,3 +82,7 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """ remove is called """
+        self.__session.remove()
