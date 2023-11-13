3373de466eca8cca12a74c76e3f98ecd70ecc200
michaellgans <michaellgans@Victoria>
2023-11-13 12:57:47 -0600

    Creates task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Task 1 """
+""" Task 2 """
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,9 +12,16 @@ def index():
 
 
 @app.route("/hbnb", strict_slashes=False)
-def index2():
+def index1():
     """ Displays string when connection is created """
     return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def index2(text):
+    """ Displays string when connection is created """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
