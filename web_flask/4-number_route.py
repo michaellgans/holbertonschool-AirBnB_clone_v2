dea15c2783bf141ab6439814be8aea3b67c3fa85
michaellgans <michaellgans@Victoria>
2023-11-13 14:02:50 -0600

    Documentation test

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -9,16 +9,25 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
+    """
+    Displays a greeting.
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def index1():
+    """
+    Displays HBNB.
+    """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def index2(text):
+    """
+    Displays C and then specific text from the user.
+    """
     text = text.replace("_", " ")
     return "C {}".format(text)
 
@@ -26,12 +35,19 @@ def index2(text):
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def index3(text="is cool"):
+    """
+    Displays Python and then is cool as default
+    This is a test
+    """
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def index3(n):
+    """
+    Using a route parameter with a converter
+    """
     return "{} is a number".format(n)
 
 
