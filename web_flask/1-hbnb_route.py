38c3a7bc7be1d58dbb589660945be9776a0dcce8
michaellgans <michaellgans@Victoria>
2023-11-13 12:41:45 -0600

    Task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,5 +11,11 @@ def index():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def index2():
+    """ Displays string when connection is created """
+    return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
