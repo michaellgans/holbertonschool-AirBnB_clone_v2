a3cdec30fbed3676a48279818eb099d9a488a0fa
michaellgans <michaellgans@Victoria>
2023-11-13 18:47:45 -0600

    pycodestyle fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,10 +11,12 @@ app = Flask(__name__)
 def teardown(exception):
     storage.close()
 
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     states = storage.all("State")
     return render_template("7-states_list.html", states=states.values())
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
