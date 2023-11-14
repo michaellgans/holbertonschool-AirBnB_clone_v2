3095074b8dc8ed93bf25bf0421f922e533a9bd83
michaellgans <michaellgans@Victoria>
2023-11-13 18:43:35 -0600

    Creates task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Task 8 """
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states.values())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
